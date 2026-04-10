66f865944064d45171a0c5fc86dacc9bcbeba2d1
Yağmur Telli <yyyagmurtelli@gmail.com>
2026-04-11 00:41:59 +0300

    Create Java-Function-Parser-Engine.java
    This repository features a robust calculation engine designed to parse and evaluate mathematical functions from string data. It demonstrates advanced string manipulation techniques, moving from static character indexing to dynamic methods like 'substring' and 'Integer.parseInt' for better data integrity. The engine handles a variety of operations, including basic arithmetic, factorials, and a complex 'sfunction' that approximates trigonometric values via recursive Taylor series expansion. The use of ListIterator for index tracking and comprehensive try-catch blocks for error handling makes this a great example of building resilient, algorithmic software that bridges the gap between raw string input and complex mathematical output.

---
+++ Java-Function-Parser-Engine.java
@@ -0,0 +1,62 @@
+import java.util.List;
+
+List<String> functiondata;
+char[] number1 = new char[2];
+char[] number2 = new char[2];
+char[] operator = new char[1];
+
+public Calculator(List<String> functiondata) {
+    this.functiondata = functiondata;
+}
+public void process() {
+    ListIterator<String> datanew = functiondata.listIterator();
+    while (datanew.hasNext()) {
+        int index = datanew.nextIndex() + 1;
+        String temp = datanew.next();
+
+        try {
+            if (temp.endsWith("!")) {
+                temp.getChars(0, 2, number1, 0);
+                int n1 = ((number1[0] - '0') * 10) + (number1[1] - '0');
+                int result = factorial(n1);
+                System.out.println(index + ". function is:" + temp + "= " + result);
+
+            } else if (temp.endsWith("s")) {
+                // Instead of getChars, use substring to safely handle "17s"
+                String numberPart = temp.substring(0, temp.length() - 1);
+                int n1 = Integer.parseInt(numberPart);
+                double result = sfunction(n1);
+                System.out.println(index + ". function is:" + temp + "= " + result);
+
+            } else {
+                temp.getChars(0, 2, number1, 0);
+                temp.getChars(2, 3, operator, 0);
+                temp.getChars(3, 5, number2, 0);
+
+                int n1 = ((number1[0] - '0') * 10) + (number1[1] - '0');
+                int n2 = ((number2[0] - '0') * 10) + (number2[1] - '0');
+                char op = operator[0];
+
+                switch (op) {
+                    case '+':
+                        System.out.println(index + ". function is:" + temp + "= " + (n1 + n2));
+                        break;
+                    case '-':
+                        System.out.println(index + ". function is:" + temp + "= " + (n1 - n2));
+                        break;
+                    case '/':
+                        if (n2 == 0) {
+                            System.out.println(index + ". function is:" + temp + "= Error zero division");
+                        } else {
+                            System.out.println(index + ". function is:" + temp + "= " + ((double) n1 / n2));
+                        }
+                        break;
+                    default:
+                        System.out.println(index + ". function is:" + temp + "= Error operator not defined");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(index + ". function is:" + temp + "= Error invalid format");
+        }
+    }
+
